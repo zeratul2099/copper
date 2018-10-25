2db0d18557592d84af47bbaf052ae756a70f28e6
Marko <zeratul@posteo.de>
2018-10-25 23:02:55 +0200

    working pixel manipulation

--- src/lib.rs
+++ src/lib.rs
@@ -18,14 +18,16 @@ pub fn load_image(filename: String) {
     let mut img = image::open(filename).unwrap().to_rgba();
 
     println!("dimensions {:?}", img.dimensions());
-
-    for i in 0..img.dimensions().0 {
-        let mut pixel = img.get_pixel(i, 100).clone();
-        println!("{:?}", pixel.data);
-        pixel.data[0] = 255;
-        img.put_pixel(i, 100, pixel);
+    for y in 90..110 {
+        for x in 0..img.dimensions().0 {
+            let mut pixel = img.get_pixel(x, y).clone();
+            println!("{:?}", pixel.data);
+            pixel.data[0] = 0;
+            pixel.data[1] = 0;
+            pixel.data[2] = 0;
+            img.put_pixel(x, y, pixel);
+        }
     }
-
     img.save("test.png").unwrap();
 }
 
