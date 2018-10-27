8dd7444aab704e5c9d253996f4e7a5560e6f23cd
Marko <zeratul@posteo.de>
2018-10-27 16:17:04 +0200

    added sha3 for upcomming use RNG for embedding

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 extern crate copper;
 extern crate image;
+extern crate sha3;
 
 use std::io;
+use sha3::{Digest, Sha3_256};
 
 fn main() {
     let mut message = String::new();
     println!("Enter message to embed:");
     io::stdin().read_line(&mut message).expect("Error reading input");
+    let hash = Sha3_256::digest(message.as_bytes());
+    println!("Sha3-hash of message is: {:x}", hash);
+
     let cover = image::open("avatar.png".to_string()).unwrap().to_rgba();
     let output = copper::lsb_embed(&cover, &message);
     output.save("test.png").unwrap();
