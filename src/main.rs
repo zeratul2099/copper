2f2a9783c3e00cec456753abddebfb9861ad7ae0
Marko <zeratul@posteo.de>
2018-10-27 16:03:04 +0200

    read line to embed

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 extern crate copper;
 extern crate image;
 
+use std::io;
+
 fn main() {
+    let mut message = String::new();
+    println!("Enter message to embed:");
+    io::stdin().read_line(&mut message).expect("Error reading input");
     let cover = image::open("avatar.png".to_string()).unwrap().to_rgba();
-    let output = copper::lsb_embed(&cover, &"My hovercraft is full of eels!".to_string());
+    let output = copper::lsb_embed(&cover, &message);
     output.save("test.png").unwrap();
     
     let msg = copper::lsb_extract(&output);
